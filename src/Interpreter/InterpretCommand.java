667f79de4a1275b6c83689b6f8de6c456ed7b991
Russell Llave <ral30@duke.edu>
2018-10-17 18:48:02 -0400

    added classes to implement for backened. added Duvall's code for program parsing

---
+++ src/Interpreter/InterpretCommand.java
@@ -0,0 +1,12 @@
+package Interpreter;
+
+public class InterpretCommand {
+    private String myCommands;
+
+    public InterpretCommand(String commands){
+        myCommands = commands;
+    }
+
+    //for testing purposes
+
+}
