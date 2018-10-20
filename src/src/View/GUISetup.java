0921e89e8bc740eb595d8c5e3846ca92261e18d6
austkao <nitsuaoak@outlook.com>
2018-10-20 06:27:57 -0400

    Minor refactoring for View

--- src/src/View/GUISetup.java
+++ src/src/View/GUISetup.java
@@ -19,8 +19,11 @@ import javafx.scene.text.Text;
 
 import java.io.File;
 import java.lang.reflect.Array;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.ResourceBundle;
 
 /**
@@ -29,6 +32,7 @@ import java.util.ResourceBundle;
  * @author Austin Kao
  */
 public class GUISetup implements FrontInternal{
+    private static final String[] LANGUAGE_ARRAY = {"Chinese","English", "French", "German", "Italian", "Portuguese", "Russian", "Spanish"};
 
     private Scene myScene;
     private GridPane myPane;
@@ -43,8 +47,6 @@ public class GUISetup implements FrontInternal{
     public Scene createGUI(int width, int height, Paint background) {
         root = new Group();
         var scene = new Scene(root, width, height, background);
-        //GridPane gp = createPane("Hi");
-        //root.getChildren().add(gp);
         turtleDisplay = new TurtleDisplay();
         myConsole = new Console();
         myConsole.getConsoleBox().setLayoutX(200);
@@ -52,12 +54,12 @@ public class GUISetup implements FrontInternal{
         ArrayList<String> languages = new ArrayList<>();
         languages.add("English");
         languages.add("Spanish");
-        HBox languageTitle = createLabel("Languages:");
+        Label languageTitle = createLabel("Languages:");
         LanguageMenu langMenu = new LanguageMenu(languages);
         ArrayList<String> colors = new ArrayList<>();
         colors.add("Black");
         colors.add("Red");
-        HBox colorTitle = createLabel("Colors:");
+        Label colorTitle = createLabel("Colors:");
         ColorMenu colorMenu = new ColorMenu(colors);
         Button startButton = createButton("Start");
         Button stopButton = createButton("Stop");
@@ -67,8 +69,6 @@ public class GUISetup implements FrontInternal{
         userOptions.setLayoutY(100);
         root.getChildren().addAll(turtleDisplay.getCanvas(), myConsole.getConsoleBox(), userOptions);
         scene.setOnKeyPressed(event -> myConsole.processCommand());
-
-
         return scene;
     }
 
@@ -77,10 +77,9 @@ public class GUISetup implements FrontInternal{
         return myScene;
     }
 
-    public HBox createLabel(String text) {
+    public Label createLabel(String text) {
         Label createdLabel = new Label(text);
-        HBox labelHBox = new HBox(createdLabel);
-        return labelHBox;
+        return createdLabel;
     }
 
 
@@ -90,11 +89,23 @@ public class GUISetup implements FrontInternal{
         return createdButton;
     }
 
-    private void createLanguageList() {
-
-    }
-
     private void changeColor(String color) {
         myScene.setFill(Paint.valueOf(color.toUpperCase()));
+    }
+
+    public ArrayList<String> listLanguages() {
+        ArrayList<String> languages = new ArrayList<>();
+        try {
+            for(int i = 0; i < LANGUAGE_ARRAY.length; i++) {
+            }
+            return languages;
+        } catch (Exception e) {
+            System.out.println("hi");
+            return languages;
+            /*catch (InstantiationException | InvocationTargetException | NoSuchMethodException | IllegalAccessException | ClassNotFoundException e) {
+            System.out.println("Cannot get languages.");
+            return languages;
+        }*/
+        }
     }
 }
