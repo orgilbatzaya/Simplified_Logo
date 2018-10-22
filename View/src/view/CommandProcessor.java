0e7f5a8275094c88b6f235d578fc53289f1f8bc6
Russell Llave <ral30@duke.edu>
2018-10-22 01:30:07 -0400

    fixed languages debugging

--- View/src/view/CommandProcessor.java
+++ View/src/view/CommandProcessor.java
@@ -11,8 +11,11 @@ public class CommandProcessor {
 
     public void processAndAnimateCommand(GUISetup gui) {
         BackMain back = new BackMain(gui.getButtonManager().getLanguageFromUserOptions(), gui.getTurtleParams());
+        System.out.println(1);
         back.performCommands(gui.getConsole().getNextCommand());
+        System.out.println(2);
         List<String> actionList = back.getMyTurtleActions();
+        System.out.println(3);
         List<Double> actionArgs = back.getMyTurtleActionsArgs();
         ActionRunner actInterpret = new ActionRunner();
         actInterpret.performActions(actionList,actionArgs,gui.getButtonManager().getTurtleDisplay());
