ad6dcf472362a606c5a1fe9837b14917b79f4342
austkao <nitsuaoak@outlook.com>
2018-10-23 23:03:01 -0400

    Added duration field

---
+++ View/src/view/fields/DurationField.java
@@ -0,0 +1,27 @@
+package view.fields;
+
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+
+public class DurationField {
+    private static final double DEFAULT_DURATION = 10;
+
+    private TextField myField;
+    private VBox myBox;
+
+    public DurationField(String label) {
+        Label fieldLabel = new Label(label);
+        myField = new TextField();
+        myField.setText(Double.toString(DEFAULT_DURATION));
+        myBox = new VBox(fieldLabel, myField);
+    }
+
+    public double getDuration() {
+        return Double.parseDouble(myField.getText());
+    }
+
+    public VBox getDisplay() {
+        return myBox;
+    }
+}
